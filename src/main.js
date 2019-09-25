94d577aa5e3f3193a8a9a0c201037832ef0f958a
Denis Shtefan <den.shtefan@gmail.com>
2019-09-25 17:01:36 +0300

    Refactoring

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
+const el = elId => document.getElementById(elId)
+
 const getCity = () => {
-  let cityName = document.getElementById("cityField").value
+  let cityName = el(`cityField`).value
   getWeather(cityName)
 }
 
 const openTab = i => {
-  let currentActiveTab = document.getElementById(`tab${activeTab}`),
-      newActiveTab = document.getElementById(`tab${i + 1}`)
+  let currentActiveTab = el(`tab${activeTab}`),
+      newActiveTab = el(`tab${i + 1}`)
   currentActiveTab.classList.remove(`active-tab`)
   newActiveTab.classList.add(`active-tab`)
   activeTab = i + 1
-  document.getElementById(`cityName`).innerText = results[0][0]
-  document.getElementById(`tabName`).innerText = results[i + 1][0]
-  document.getElementById(`result`).innerText = results[i + 1][1]
-  document.getElementById(`unit`).innerText = results[i + 1][2]
-  document.getElementById(`weatherIcon`).innerHTML = `<img src="img/weather_icons/${results[i + 1][3]}.svg">`
+  el(`cityName`).innerText = results[0][0]
+  el(`tabName`).innerText = results[i + 1][0]
+  el(`result`).innerText = results[i + 1][1]
+  el(`unit`).innerText = results[i + 1][2]
+  el(`weatherIcon`).innerHTML = `<img src="img/weather_icons/${results[i + 1][3]}.svg">`
   displayIcon(``)
 }
 
 const getWeather = cityName => {
-  axios.get('https://api.openweathermap.org/data/2.5/weather?', {
+  axios.get(`https://api.openweathermap.org/data/2.5/weather?`, {
     params: {
       q: cityName,
       appid: APIkey
@@ -39,7 +41,7 @@ const getWeather = cityName => {
 const fillingWeatherInfo = weather => {
   let city = weather.name, 
       main = weather.weather[0].main, 
-      temp = (weather.main.temp - kelvin).toFixed(0) , 
+      temp = (weather.main.temp - kelvin).toFixed(0), 
       max_temp = (weather.main.temp_max - kelvin).toFixed(0), 
       min_temp = (weather.main.temp_min - kelvin).toFixed(0), 
       wind = weather.wind.speed, 
@@ -49,8 +51,8 @@ const fillingWeatherInfo = weather => {
       pressure = weather.main.pressure, 
       timezone = weather.timezone / 3600,
       icon = weather.weather[0].icon.substr(0, 2)
-  const getDate = (d) => 
-    `${d.getUTCHours() + timezone}:${d.getUTCMinutes()+ timezone}`
+  const getDate = d => 
+    `${d.getUTCHours() + timezone}:${d.getUTCMinutes() + timezone}`
   
   results = [
     [city],
@@ -63,10 +65,10 @@ const fillingWeatherInfo = weather => {
 }
 
 const errorMessage = message => {
-  document.getElementById(`cityName`).innerText = ``
-  document.getElementById(`result`).innerText = ``
-  document.getElementById(`unit`).innerText = ``
-  document.getElementById(`tabName`).innerText = message
+  el(`cityName`).innerText = ``
+  el(`result`).innerText = ``
+  el(`unit`).innerText = ``
+  el(`tabName`).innerText = message
 }
 
 const lockTabs = () => {
@@ -79,19 +81,19 @@ const unlockTabs = () =>
 
 const displayIcon = mode => {
   if (mode === `none`) {
-    document.getElementById("weatherIcon").style.display = `none`
-    document.getElementById("verticalLine").style.display = `none`
+    el(`weatherIcon`).style.display = `none`
+    el(`verticalLine`).style.display = `none`
   } else {
-    document.getElementById("weatherIcon").style.display = `block`
-    document.getElementById("verticalLine").style.display = `block`
+    el(`weatherIcon`).style.display = `block`
+    el(`verticalLine`).style.display = `block`
   }
 }
 
 let results = [[]], 
     kelvin = 273.15, 
-    APIkey = "d3fb6b0837add2d07e9d69ef97b85afd", 
+    APIkey = `d3fb6b0837add2d07e9d69ef97b85afd`, 
     weather, 
     activeTab = 1, 
-    tabs = document.getElementsByClassName("tab")
-document.getElementById("searchButton").onclick = getCity
+    tabs = document.getElementsByClassName(`tab`)
+el(`searchButton`).onclick = getCity
 displayIcon(`none`)
