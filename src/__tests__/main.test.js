1c35e416f62e2a49626ea00f95d16846799e8294
Denis <den.shtefan@gmail.com>
2019-11-29 05:39:22 +0300

    Add jest

---
+++ src/__tests__/main.test.js
@@ -0,0 +1,8 @@
+import { weatherDataDestructuring } from '../js/weatherDataDestructuring';
+import { getCity } from "../js/getCity";
+import { getWeather } from "../js/getWeather";
+import { openTab } from "../js/openTab";
+import {addTemplate, displayIcon, preparingData, preparingErrorMessage} from "../js/renderingFunctions";
+
+test('1', () => {
+});
